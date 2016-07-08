87392d2921f01631b0192de20597a03b7ef2d2f4
Anthony Shaw <anthonyshaw@apache.org>
2016-07-08 13:55:59 +1000

    add mapping feature

--- rightscale.py
+++ rightscale.py
@@ -2,14 +2,31 @@ from libcloud.compute.providers import get_driver
 import yaml
 
 import logging
-logging.basicConfig(filename='libcloud.log',level=logging.DEBUG)
+logging.basicConfig(filename='libcloud.log', level=logging.DEBUG)
 
-def get_connection():
+
+def get_configuration():
     config = {}
     with open('config.yaml', 'r') as file:
         config_text = file.read()
         config = yaml.load(config_text)
+    return config
 
+
+def get_connection(config=None):
+    if config is None:
+        config = get_configuration()
     cls = get_driver(config['provider'])
     driver = cls(**config['connection'])
     return driver
+
+
+def map(config, map_name, dictionary, extra_dictionary):
+    if 'maps' not in config.keys():
+        return
+    if map_name not in config['maps'].keys():
+        return
+    for key, value in config['maps'][map_name].items():
+        mapped_value = extra_dictionary.get(value, None)
+        dictionary[key] = mapped_value
+    return dictionary
